58fcce012444d67db17651e8166a6b79e5d27896
Владимир <kbobah@yandex.ru>
2020-04-12 20:20:52 +0300

    Spreadsheet работает 100%

--- 1D Spreadsheet.cpp
+++ 1D Spreadsheet.cpp
@@ -2,29 +2,47 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
-
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
+int refer(string);
+class Stroka
+{
+public:
+    string oper;
+    string a1;
+    string a2;
+    int rezultat;
+    bool r = false;// флаг подсчитанного результата
+    int rez()
+    {
+        if (r == true) return rezultat;// если уже считали, то повторно не считать
+        int rezult = 0;
+        if (oper == "VALUE") rezult = refer(a1);
+        else if (oper == "ADD") rezult = refer(a1) + refer(a2);
+        else if (oper == "SUB") rezult = refer(a1) - refer(a2);
+        else if (oper == "MULT") rezult = refer(a1) * refer(a2);
+        r = true;
+        rezultat = rezult;
+        return rezultat;
+    }
+};
+vector<Stroka> sh1;
+int refer(string st)
+{
+    if (st == "_") return 0;
+    if (st[0] != '$') return stoi(st);
+    int n = stoi(st.substr(1));
+    return sh1[n].rez();
+}
 int main()
 {
     int N;
     cin >> N; cin.ignore();
     for (int i = 0; i < N; i++) {
-        string operation;
-        string arg1;
-        string arg2;
-        cin >> operation >> arg1 >> arg2; cin.ignore();
+        Stroka x;
+        cin >> x.oper >> x.a1 >> x.a2; cin.ignore();
+        sh1.push_back(x);
     }
     for (int i = 0; i < N; i++) {
-
-        // Write an answer using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
-
-        cout << "1" << endl;
+        cout << sh1[i].rez() << endl;
     }
 }
